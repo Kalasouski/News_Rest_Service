3a6f31a9809565c0a9b09f9fe8869b2841fefbe5
Kalasouski <kolosovsky2002@gmail.com>
2021-07-30 16:45:03 +0300

    initial spring boot project
    -forgot to add security dependency

---
+++ demo/news-rest-service/src/main/java/by/itechart/newsrestservice/NewsRestServiceApplication.java
@@ -0,0 +1,13 @@
+package by.itechart.newsrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsRestServiceApplication.class, args);
+    }
+
+}
---
+++ demo/news-rest-service/src/test/java/by/itechart/newsrestservice/NewsRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.itechart.newsrestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsRestServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
