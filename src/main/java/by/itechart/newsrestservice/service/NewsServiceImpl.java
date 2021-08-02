833290682195e4762cdd1abe947118e48355117a
Sergey Lemesh <serrgey.l@gmail.com>
2021-08-02 11:50:58 +0300

    method findByCategory added

--- src/main/java/by/itechart/newsrestservice/service/NewsServiceImpl.java
+++ src/main/java/by/itechart/newsrestservice/service/NewsServiceImpl.java
@@ -1,9 +1,12 @@
 package by.itechart.newsrestservice.service;
 
 import by.itechart.newsrestservice.entity.News;
+import by.itechart.newsrestservice.entity.NewsCategory;
 import by.itechart.newsrestservice.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NewsServiceImpl {
@@ -16,5 +19,9 @@ public class NewsServiceImpl {
 
     News findById(Long id) {
         return newsRepository.getById(id);
+    }
+
+    List<News> findByCategory(NewsCategory category) {
+        return newsRepository.findByCategory(category);
     }
 }
