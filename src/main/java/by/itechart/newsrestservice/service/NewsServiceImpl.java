d719ba2096facb470272cc60ebe2b7bb6add992f
Kalasouski <kolosovsky2002@gmail.com>
2021-08-02 11:40:27 +0300

    Update NewsServiceImpl.java

--- src/main/java/by/itechart/newsrestservice/service/NewsServiceImpl.java
+++ src/main/java/by/itechart/newsrestservice/service/NewsServiceImpl.java
@@ -1,12 +1,15 @@
 package by.itechart.newsrestservice.service;
 
 import by.itechart.newsrestservice.entity.News;
+import by.itechart.newsrestservice.entity.NewsCategory;
 import by.itechart.newsrestservice.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class NewsServiceImpl {
+public class NewsServiceImpl implements NewsService {
     private final NewsRepository newsRepository;
 
     @Autowired
@@ -14,7 +17,23 @@ public class NewsServiceImpl {
         this.newsRepository = newsRepository;
     }
 
-    News findById(Long id) {
+    public News findById(Long id) {
         return newsRepository.getById(id);
+    }
+
+    @Override
+    public List<News> findByCategory(NewsCategory category) {
+        return null;
+    }
+
+    //my impl
+    @Override
+    public List<News> findAll() {
+        return newsRepository.findAll();
+    }
+
+    @Override
+    public News save(News news) {
+        return null;
     }
 }
