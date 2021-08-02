6598296c450bf2c9c54c23e6e987347376057b14
Sergey Lemesh <serrgey.l@gmail.com>
2021-08-02 12:23:13 +0300

    minor bugs fixed

--- src/main/java/by/itechart/newsrestservice/service/NewsServiceImpl.java
+++ src/main/java/by/itechart/newsrestservice/service/NewsServiceImpl.java
@@ -1,9 +1,12 @@
 package by.itechart.newsrestservice.service;
 
 import by.itechart.newsrestservice.entity.News;
+import by.itechart.newsrestservice.entity.NewsCategory;
 import by.itechart.newsrestservice.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NewsServiceImpl {
