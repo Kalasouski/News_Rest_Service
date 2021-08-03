47c3b32486a2f433485112f0b2e5ec4fd3795de0
Kalasouski <kolosovsky2002@gmail.com>
2021-08-03 11:31:31 +0300

    Create NewsController.java

---
+++ src/main/java/by/itechart/newsrestservice/controller/NewsController.java
@@ -0,0 +1,39 @@
+package by.itechart.newsrestservice.controller;
+
+import by.itechart.newsrestservice.entity.News;
+import by.itechart.newsrestservice.entity.User;
+import by.itechart.newsrestservice.exceptions.InvalidIdException;
+import by.itechart.newsrestservice.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@RestController
+public class NewsController {
+    private final NewsService newsService;
+
+    @Autowired
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @GetMapping("/news")
+    @ResponseBody
+    public Map<Integer, String> getNewsHeadings() {
+        Map<Integer, String> headers = new HashMap<>();
+        List<News> news = newsService.findAll();
+        for(int i = 0;i<news.size();i++) {
+            headers.put(i+1,news.get(i).getHeading());
+        }
+        return headers;
+    }
+}
