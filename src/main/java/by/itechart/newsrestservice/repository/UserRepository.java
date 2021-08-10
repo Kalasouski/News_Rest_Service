6bf90ed52231a82b53c411bde8a0dcf381dca30c
Sergey Lemesh <serrgey.l@gmail.com>
2021-08-10 12:12:20 +0300

    irrelevant methods deleted in repositories

--- src/main/java/by/itechart/newsrestservice/repository/UserRepository.java
+++ src/main/java/by/itechart/newsrestservice/repository/UserRepository.java
@@ -3,12 +3,7 @@ package by.itechart.newsrestservice.repository;
 import by.itechart.newsrestservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-
-
-    List<User> findAll();
-
 }
