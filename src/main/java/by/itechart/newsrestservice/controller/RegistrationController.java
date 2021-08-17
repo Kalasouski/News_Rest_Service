2ceb7d3baa3bcb7a3f3864939aca884799b0c06c
Sergey Lemesh <serrgey.l@gmail.com>
2021-08-17 12:29:45 +0300

    Registration feature addded

---
+++ src/main/java/by/itechart/newsrestservice/controller/RegistrationController.java
@@ -0,0 +1,26 @@
+package by.itechart.newsrestservice.controller;
+
+import by.itechart.newsrestservice.dto.RegistrationRequestDto;
+import by.itechart.newsrestservice.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class RegistrationController {
+    private final UserService userService;
+    private static final String REGISTRATION_ERROR_MESSAGE = "User already exists! Try new username.";
+    private static final String SUCCESSFULLY_REGISTRATION_MESSAGE = "User successfully registered!";
+
+    @PostMapping("/signUp")
+    public ResponseEntity<String> addNewUser(@RequestBody RegistrationRequestDto registrationRequestDto) {
+        if (!userService.addNewUser(registrationRequestDto)) {
+            return new ResponseEntity<>(REGISTRATION_ERROR_MESSAGE, HttpStatus.CONFLICT);
+        }
+        return ResponseEntity.ok(SUCCESSFULLY_REGISTRATION_MESSAGE);
+    }
+}
---
+++ src/main/java/by/itechart/newsrestservice/dto/RegistrationRequestDto.java
@@ -0,0 +1,25 @@
+package by.itechart.newsrestservice.dto;
+
+import by.itechart.newsrestservice.entity.Role;
+import by.itechart.newsrestservice.entity.Status;
+import by.itechart.newsrestservice.entity.User;
+import lombok.Data;
+
+@Data
+public class RegistrationRequestDto {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User dtoToUser() {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setRole(Role.ROLE_USER);
+        user.setStatus(Status.ACTIVE);
+        return user;
+    }
+}
